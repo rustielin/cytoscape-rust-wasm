7edfc442886dec8fe3a70414754bf1470d7cb378
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-01 00:56:16 +0000

    Deploy rustielin/cytoscape-rust-wasm to github.com/rustielin/cytoscape-rust-wasm.git:gh-pages

--- bootstrap.js
+++ bootstrap.js
@@ -180,7 +180,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/wasm_cytoscape_bg.wasm":"791b392f9baf2c52c6fd"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/wasm_cytoscape_bg.wasm":"1a4fbf1cfca07604fbd3"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
